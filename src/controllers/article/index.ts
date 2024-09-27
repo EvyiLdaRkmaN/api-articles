47e0a1b837af1dba5fc6b05dc3ae2ceadf9ae6b9
Richard Yordy <evil.darkman@hotmail.com>
2024-09-27 15:53:46 -0600

    feat: add updateArticle method with field validation for article updates

--- src/controllers/article/index.ts
+++ src/controllers/article/index.ts
@@ -1,5 +1,6 @@
 import Article from '@models/article';
 import { Request, Response } from 'express';
+import { ValidationError } from 'sequelize';
 
 
 
@@ -16,6 +17,7 @@ export const getArticles = async (req: Request, res: Response) => {
 export const createArticle = async (req: Request, res: Response) => {
   const { id, name, description, price, model } = req.body;
   try {
+    
     const article = await Article.create({
       id,
       name,
@@ -26,11 +28,52 @@ export const createArticle = async (req: Request, res: Response) => {
     res.status(201).json(article);
 
   } catch (error) {
-    
+    console.log('error', error);
     res.status(500).json(error);
   }
 };
 
+export const updateArticle = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { ...fields } = req.body;
 
+  try {
+
+    const allowedFields = Object.keys(Article.getAttributes());
+    const invalidFields = Object.keys(fields).filter(field => !allowedFields.includes(field));
+
+    if (invalidFields.length > 0) {
+      throw new Error(`campos no validos: ${invalidFields.join(', ')}`, {
+        cause: 'Invalid fields',
+      });
+    }
+
+    const article = await Article.findByPk(id);
+
+    if (article) {
+      await article.update(
+        { ...fields },
+      );
+      res.status(200).json(article);
+      return;
+    }
+
+    res.status(404).json({ message: 'Articulo no encontrado' });
+
+  } catch (error) {
+    console.error('error', error);
+    if (error instanceof Error && error.cause === 'Invalid fields') {
+      res.status(400).json({ message: error.message });
+      return;
+    }
+
+    if (error instanceof Error && error.cause === 'validation') {
+      res.status(400).json({ message: error.message });
+      return;
+    }
+
+    res.status(500).json({ message: 'Ocurrio un error inesperado' });
+  }
+};
 
 
