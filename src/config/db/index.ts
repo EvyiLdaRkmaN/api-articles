82ea12bbe73d33177c0a6bac8bd0151f7f72da96
Richard Yordy <evil.darkman@hotmail.com>
2024-10-07 19:19:35 -0600

    feat: add Cloud SQL Unix socket configuration for database connection

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -9,14 +9,19 @@ const dbpassword = process.env.DB_PASSWORD || '';
 const dbhost = process.env.DB_HOST || 'localhost';
 
 
+
 console.log('password', dbpassword);
 console.log('host', dbhost);
 console.log('user', dbuser);
 console.log('name', dbname);
+console.log('socket', process.env.INSTANCE_UNIX_SOCKET);
 
 const db = new Sequelize(dbname, dbuser, dbpassword, {
   host: dbhost,
   dialect: 'mysql',
+  dialectOptions: {
+    socketPath: process.env.INSTANCE_UNIX_SOCKET
+  },
   define: {
     timestamps: false,
     freezeTableName: true
